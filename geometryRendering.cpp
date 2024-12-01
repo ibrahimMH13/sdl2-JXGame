4f3c5ecf2eb529d9b11eea7e77f2cd6799c1a2e9
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-12-01 10:50:44 +0800

    fx

--- geometryRendering.cpp
+++ geometryRendering.cpp
@@ -1,54 +1,50 @@
 #include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <string>
 #include <iostream>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+SDL_Renderer* gRenderer;
+SDL_Window* window;
+void close(){
+    
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-bool loadMedia() {
-    bool success = true;
-
-    return success;
 }
-
 int main() {
-    bool quit = false;
-    SDL_Event e;
-
-    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        std::cerr << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    // Create window
-    SDL_Window* window = SDL_CreateWindow(
-        "Guessing Game",
+        window = SDL_CreateWindow(
+        "Rectangle Test",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         SCREEN_WIDTH,
         SCREEN_HEIGHT,
         SDL_WINDOW_SHOWN);
 
-    if (window == NULL) {
-        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return -1;
     }
 
-    // Create renderer
-    SDL_Renderer* gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (gRenderer == NULL) {
-        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+    gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    
+    if (gRenderer == nullptr) {
+        std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return -1;
     }
 
-    // Set initial render draw color (background color)
     SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF); // Black background
+
+    bool quit = false;
+    SDL_Event e;
 
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
@@ -57,27 +53,35 @@ int main() {
             }
         }
 
-        // Clear the screen
+        SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
         SDL_RenderClear(gRenderer);
 
         // Render red filled rectangle
         SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);        
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
         SDL_RenderFillRect(gRenderer, &fillRect);
 
-        // Render green outline rectangle
+        // Render green outline rectangle (larger, surrounding the red filled rectangle)
         SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
-        SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);        
+        SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
         SDL_RenderDrawRect(gRenderer, &outlineRect);
 
-        // Update screen
+        // Draw blue horizontal line
+        SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );        
+        SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 );
+
+        // yellow 
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
+        for (int i = 0; i < SCREEN_HEIGHT; i+=4)
+        {
+            SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2 , i);
+        }
+        
+        // Update the screen
         SDL_RenderPresent(gRenderer);
     }
 
-    // Clean up
-    SDL_DestroyRenderer(gRenderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    close();
 
     return 0;
 }
